9f3d3407f85d2ef91d7248aa049f50a1f2414d54
Stefan Fischer <sfischer13@ymail.com>
2017-06-05 02:01:00 +0200

    Reorder setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 
-
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -13,29 +11,13 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [
-]
+requirements = ['numpy']
 
-test_requirements = [
-]
+test_requirements = []
 
 setup(
-    name='stringmetric',
-    version='0.1.0',
-    description='Python implementations of common string distance and similarity algorithms.',
-    long_description=readme + '\n\n' + history,
     author='Stefan Fischer',
     author_email='sfischer13@ymail.com',
-    url='https://github.com/sfischer13/python-stringmetric',
-    packages=[
-        'stringmetric',
-    ],
-    package_dir={'stringmetric': 'stringmetric'},
-    include_package_data=True,
-    install_requires=requirements,
-    license='MIT',
-    zip_safe=False,
-    keywords='string metric distance similarity phonetic hamming',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -49,12 +31,23 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: Implementation',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing',
         'Topic :: Text Processing :: Linguistic',
     ],
+    description='Python implementation of common string distance and similarity algorithms.',
+    include_package_data=True,
+    install_requires=requirements,
+    keywords='string metric distance similarity phonetic hamming levenshtein',
+    license='MIT',
+    long_description=readme + '\n\n' + history,
+    name='stringmetric',
+    package_dir={'stringmetric': 'stringmetric'},
+    packages=['stringmetric'],
     test_suite='tests',
-    tests_require=test_requirements
+    tests_require=test_requirements,
+    url='https://github.com/sfischer13/python-stringmetric',
+    version='0.2.0',
+    zip_safe=False
 )
